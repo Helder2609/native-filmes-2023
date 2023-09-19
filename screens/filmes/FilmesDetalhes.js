e0aa63ca2dd98c089fbec9d28af93b1a6ff65450
Helder2609 <helder.muniz@iesb.edu.br>
2023-09-18 22:12:37 -0300

    project

--- screens/filmes/FilmesDetalhes.js
+++ screens/filmes/FilmesDetalhes.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Text } from 'react-native-paper'
+import { Card, Text } from 'react-native-paper'
 import apiFilmes from '../../services/apiFilmes'
 
 const FilmesDetalhes = ({ navigation, route }) => {
@@ -8,13 +8,30 @@ const FilmesDetalhes = ({ navigation, route }) => {
 
     useEffect(() => {
         const id = route.params.id
-        apiFilmes.get(`/movie/${id}?language=pt-BR`).then(resultado => {
+        apiFilmes.get(`/movie/${id}?`).then(resultado => {
             setFilme(resultado.data)
         })
     }, [])
 
     return (
-        <Text>Id: {route.params.id} </Text>
+        <>
+        <Card>
+            <Card.Cover source={{ uri: 'https://image.tmdb.org/t/p/w500/' + item.backdrop_path }} />
+            <Card.Content>
+                <Text variant="titleLarge">{filme.title}</Text>
+                <Text variant="bodyMedium">{filme.overview}</Text>
+            </Card.Content>
+        </Card >
+        <Card>
+            <Card.Cover source={{ uri: 'https://image.tmdb.org/t/p/w500/' + item.backdrop_path }} />
+            <Card.Content>
+                <Text variant="titleLarge">Popularidade{filme.popularity}</Text>
+                <Text variant="bodyMedium">{filme.overview}</Text>
+                <Text variant="bodyMedium">{filme.overview}</Text>
+                <Text variant="bodyMedium">{filme.overview}</Text>
+            </Card.Content>
+        </Card >
+      </>
     )
 }
 
--- services/apiFilmes.js
+++ services/apiFilmes.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 const apiFilmes = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
+    params: {
+        language: 'pt-BR'
+    },
     headers: {
-        Authorization: 'Bearer '
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYWM5MDA2ZTQ1NWYyYzRjOWY5ZTA1NGMwNmQxZTQwMiIsInN1YiI6IjY0ZWZlMmM1NzJjMTNlMDEzOWIzNGVlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5LMZB2PJkaqeQOSTa7muZU8YDQ_dOb4_jtHiXy4HYAc'
     }
 })
 
